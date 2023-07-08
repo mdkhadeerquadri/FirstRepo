c3db441f54f8dc4381c66c8ec7d14615fe12b885
mdkhadeerquadri <138885678+mdkhadeerquadri@users.noreply.github.com>
2023-07-08 11:11:28 +0530

    new method add

--- Git_Project_1/src/main/java/TestClass.java
+++ Git_Project_1/src/main/java/TestClass.java
@@ -2,6 +2,10 @@
 public class TestClass {
 	public static void m() {
 		System.out.println("first change");
+		System.out.println("second change");
+	}
+	public void m2() {
+		System.out.println("m2 first change");
 	}
 
 }
