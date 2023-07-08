6c0df78d6175f4771eb8dc0a9ef8009b771dff05
mdkhadeerquadri <138885678+mdkhadeerquadri@users.noreply.github.com>
2023-07-08 12:36:29 +0530

    remove methods

--- Git_Project_1/src/main/java/TestClass.java
+++ Git_Project_1/src/main/java/TestClass.java
@@ -1,18 +1,5 @@
 
 public class TestClass {
-	public static void m() {
-		System.out.println("first change");
-		System.out.println("second change");
-	}
-	public void m2() {
-		System.out.println("m2 first change");
-		System.out.println("m2 second change");
-		
-	}
-	private void m3() {
-		System.out.println("m3 first change");
-		System.out.println("m3 second change");
-
-	}
+	
 
 }
